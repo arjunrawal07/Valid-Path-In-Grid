7e0cd434a5b71400e309bd41dcfe88180d0172ae
ARJUN_RAWAL <arjunrawal07@gmail.com>
2020-07-13 11:20:18 -0400

    changed conditions to reflect what grid[i][j] should equal to return false rather than what it should equal to return true

--- script.js
+++ script.js
@@ -4,45 +4,45 @@ let grid = [[], []];
 let connectPaths = function () {
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[i].length; j++) {
-      if (grid[i][j] == 1 && (grid[i][j + 1] == 1 || 3 || 4 || 5 || 6)) {
-        pathChecks.push("true");
+      if (grid[i][j] == 1 && grid[i][j + 1] == 2) {
+        pathChecks.push("false");
         //   } else {
         //     pathChecks.push("false");
         //   }
-      } else if (grid[i][j] == 2 && (grid[i][j + 1] == 2 || 3 || 4 || 5 || 6)) {
-        pathChecks.push("true");
+      } else if (grid[i][j] == 2 && grid[i][j + 1] == 1) {
+        pathChecks.push("false");
         //   } else {
         //     pathChecks.push("false");
         //   }
-      } else if (grid[i][j] == 3 && (grid[i][j + 1] == 1 || 2 || 4 || 5 || 6)) {
-        pathChecks.push("true");
+      } else if (grid[i][j] == 3 && grid[i][j + 1] == 3) {
+        pathChecks.push("false");
         //   } else {
         //     pathChecks.push("false");
         //   }
-      } else if (grid[i][j] == 4 && (grid[i][j + 1] == 1 || 2 || 3 || 5 || 6)) {
-        pathChecks.push("true");
+      } else if (grid[i][j] == 4 && grid[i][j + 1] == 4) {
+        pathChecks.push("false");
         //   } else {
         //     pathChecks.push("false");
         //   }
-      } else if (grid[i][j] == 5 && (grid[i][j + 1] == 1 || 2 || 3 || 4 || 6)) {
-        pathChecks.push("true");
+      } else if (grid[i][j] == 5 && grid[i][j + 1] == 5) {
+        pathChecks.push("false");
         //   } else {
         //     pathChecks.push("false");
         //   }
-      } else if (grid[i][j] == 6 && (grid[i][j + 1] == 1 || 2 || 3 || 4 || 5)) {
-        pathChecks.push("true");
+      } else if (grid[i][j] == 6 && grid[i][j + 1] == 6) {
+        pathChecks.push("false");
         //   } else {
         //     pathChecks.push("false");
         //   }
       } else {
-        pathChecks.push("false");
+        pathChecks.push("true");
       }
     }
   }
 };
 var hasValidPath = function (pathChecks) {
   for (let i = 0; i < pathChecks.length; i++) {
-    if (pathChecks[i] === "false") {
+    if (pathChecks[i] == "false") {
       return "false";
     } else {
       return "true";
